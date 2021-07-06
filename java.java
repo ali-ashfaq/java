8c2975980972c161ad79036b68344accdd439cb2
ali-ashfaq <87034483+ali-ashfaq@users.noreply.github.com>
2021-07-06 23:22:17 +0530

    Add files via upload

---
+++ java.java
@@ -0,0 +1,37 @@
+package test;
+//int--All Numbers 
+//char-Alphabets
+//float-decimal
+
+public class Alma {
+
+	public static void main(String[] args) {
+		byte b=20;
+		byte c=2;
+		int myint=b*c;
+		System.out.println(myint);
+		byte d=(byte)(b*c);
+		System.out.println(d);
+		int myintvalue=257;
+		byte mybitevalue=(byte)myintvalue;
+		System.out.println(mybitevalue);
+		
+		int blala=b;
+		float f=120.35f;
+		
+		int myintfloat=(int)f;
+		System.out.println(myintfloat);
+		
+		f=myintvalue;
+		char ch='A';
+		System.out.println((int)ch);
+		System.out.println((char)69);
+
+
+
+
+
+		
+	}
+
+}
